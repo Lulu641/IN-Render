c6228812f114eaf8e6b165a34fba2eddde6e9509
Lulu641 <126349953+Lulu641@users.noreply.github.com>
2024-03-03 19:51:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
     Luffy: 'GumGum',
     Luna: 'Spence',
     Hogan: 'Mopo',
+    JW: 'Bucs',
+    Naruto: 'Sasuke',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
